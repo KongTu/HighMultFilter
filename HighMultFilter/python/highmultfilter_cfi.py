21569663212c01b3bcf678309e54f2c1d6d47152
Kong <kongPro@Kongs-MacBook-Pro.local>
2016-05-15 15:31:20 +0200

    add filter

---
+++ HighMultFilter/python/highmultfilter_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+highMultFilter = cms.EDFilter('HighMultFilter',
+                                  multMax = cms.double(100000),
+                                  multMin = cms.double(120),
+                                  etaMax = cms.double(2.4),
+                                  etaMin = cms.double(-2.4),
+                                  doGenParticle = cms.bool(False)
+                                  )
